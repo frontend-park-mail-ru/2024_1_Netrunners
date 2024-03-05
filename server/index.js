696dd0c1ebc94da7dea34120b33fbc05895adcfa
DeadBread-001 <nikita10062004m@gmail.com>
2024-03-06 00:02:09 +0300

    fixed front port

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8080;
 const ipAddress = '94.139.247.246';
 
 app.listen(port, ipAddress, function () {
