17faa75eb818db2c7b710532e795b48a7dc5d5c6
DeadBread-001 <nikita10062004m@gmail.com>
2024-04-07 21:23:26 +0300

    [42] Переделал главную страницу

---
+++ src/utils/transformers/filmDataWithDuration.js
@@ -0,0 +1,8 @@
+import {timeConvert} from '../timeConvert.js';
+
+export const toFilmDataWithDuration = (filmData) => {
+  return filmData.map((film) => ({
+    ...film,
+    duration: timeConvert.timeIntoText(film.duration),
+  }));
+};
