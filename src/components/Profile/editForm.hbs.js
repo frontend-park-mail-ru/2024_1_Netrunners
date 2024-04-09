16e88b14ec0e3907d68535e159acfc237fa46147
DeadBread-001 <nikita10062004m@gmail.com>
2024-04-09 21:09:27 +0300

    вывод ошибок

--- src/components/Profile/editForm.hbs.js
+++ src/components/Profile/editForm.hbs.js
@@ -2,19 +2,23 @@ export const editFormTemplate = `
     <form id="myForm">
         <h1>Смена аватара</h1>
         <div class="edit-section">
-            <div class="error-avatar" id="avatar-errors"></div>
-            <input type="file" id="imageInput" placeholder="Выберите изображение" name="avatar">
+            <div class="edit-form">
+                <div class="error-avatar" id="avatar-errors"></div>
+                <input type="file" id="imageInput" placeholder="Выберите изображение" name="avatar">
+            </div>
             <button type="button" id="sendImageBtn" class="edit-form-button">Сохранить</Сохранить></button>
         </div>
         <h1>Смена логина</h1>
         <div class="edit-section">
-            <div class="error-username" id="username-errors"></div>
-            <input type="text" id="loginInput" placeholder="Введите логин" name="username">
+            <div class="edit-form">
+                <div class="error-username" id="username-errors"></div>
+                <input type="text" id="loginInput" placeholder="Введите логин" name="username">
+            </div>
             <button type="button" id="sendLoginBtn" class="edit-form-button">Сохранить</button>
         </div>
         <h1>Смена пароля</h1>
         <div class="edit-section">
-            <div class="password-section">
+            <div class="edit-form">
                 <div class="error-password" id="password-errors"></div>
                 <input type="password" id="passwordInput" placeholder="Введите пароль" name="password">
                 <input type="password" id="confirmPasswordInput" placeholder="Подтвердите пароль", name="passConf">
