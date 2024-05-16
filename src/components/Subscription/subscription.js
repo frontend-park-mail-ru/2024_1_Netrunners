ea31ebfb9db395add447190e6b798bf68aae3f16
DeadBread-001 <nikita10062004m@gmail.com>
2024-05-16 11:06:05 +0300

    начало деплоя

--- src/components/Subscription/subscription.js
+++ src/components/Subscription/subscription.js
@@ -6,4 +6,17 @@ import template from "./subscription.hbs";
  */
 export function renderSubscriptionPage() {
   document.querySelector("main").innerHTML = template();
+
+  const monthlyButton = document.querySelector("#monthly");
+  const yearlyButton = document.querySelector("#yearly");
+
+  monthlyButton.addEventListener("click", () => {
+    console.log("месяц");
+    // TODO оплата ежемесячной подписки
+  });
+
+  yearlyButton.addEventListener("click", () => {
+    console.log("год");
+    // TODO оплата ежегодной подписки
+  });
 }
